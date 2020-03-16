fbd502ca4039af6e43f358e2493fcf09a3daf4f7
Casey Law <claw@dsamaster.ovro.pvt>
2020-03-16 15:16:05 -0700

    adding test and deploying data

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(name='dsa110-antpos',
       url='http://github.com/dsa110/dsa110-antpos/',
       packages=['antpos'],
-      package_data={'antpos':['data/*.csv']},
+      package_data={
+          'antpos': ['data/DSA110_positions_RevD.csv']},
       zip_safe=False)
 
