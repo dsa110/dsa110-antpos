c16d9766598b07ed8e4587cf28fb9b01f670ff52
= Casey Law <= caseyjlaw@gmail.com>
2020-04-02 13:23:34 -0700

    adding tag and github versioning

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from setuptools import setup
+from version import get_git_version
 
 setup(name='dsa110-antpos',
-      version='0.1',
+      version=get_git_version(),
       url='http://github.com/dsa110/dsa110-antpos/',
       packages=['antpos'],
       package_data={
