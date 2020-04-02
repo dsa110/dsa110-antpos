3d3e2e6391715eca7eb88cd90215b81b746fc133
= Casey Law <= caseyjlaw@gmail.com>
2020-04-02 10:23:07 -0700

    adding fixed version

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup
 
 setup(name='dsa110-antpos',
+      version='0.1',
       url='http://github.com/dsa110/dsa110-antpos/',
       packages=['antpos'],
       package_data={
