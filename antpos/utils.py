31a5aa4551f0f5d653f546276472e3390ec4bd15
dsa <dsa@github.com>
2020-09-01 19:17:03 +0000

    allow user-defined array center

--- antpos/utils.py
+++ antpos/utils.py
@@ -35,24 +35,29 @@ def get_lonlat(csvfile='{0}/data/DSA110_positions_RevE.csv'.format(antpos.__path
     return df
 
 def get_itrf(csvfile='{0}/data/DSA110_positions_RevE.csv'.format(antpos.__path__[0]),
-             headerline=13,height=1222*u.m):
-    """Read positions of all antennas from DSA110 CSV file and 
-    convert to ITRF coordinates."""
+             headerline=13, height=None, latlon_center=None):
+    """Converts antenna positions from `csvfile` to ITRF coordinates. 
+    """
+    if height is None:
+        height = 1222*u.m
+    if latlon_center is None:
+        (latcenter, loncenter) = tee_centers()
+    else:
+        (latcenter, loncenter) = latlon_center
 
-    df = get_lonlat(csvfile,headerline)
-    (latcenter,loncenter) = tee_centers()
-    center = EarthLocation(lat=latcenter,lon=loncenter,height=height)
-    df['x_m'] = EarthLocation(lat=df['Latitude'],lon=df['Longitude'],height=height).x.to_value(u.m)
-    df['y_m'] = EarthLocation(lat=df['Latitude'],lon=df['Longitude'],height=height).y.to_value(u.m)
-    df['z_m'] = EarthLocation(lat=df['Latitude'],lon=df['Longitude'],height=height).z.to_value(u.m)
+    df = get_lonlat(csvfile, headerline)
+    center = EarthLocation(lat=latcenter, lon=loncenter, height=height)
+    df['x_m'] = EarthLocation(lat=df['Latitude'], lon=df['Longitude'], height=height).x.to_value(u.m)
+    df['y_m'] = EarthLocation(lat=df['Latitude'], lon=df['Longitude'], height=height).y.to_value(u.m)
+    df['z_m'] = EarthLocation(lat=df['Latitude'], lon=df['Longitude'], height=height).z.to_value(u.m)
 
-    df['dx_m'] = (EarthLocation(lat=df['Latitude'],lon=df['Longitude'],height=height).x - center.x).to_value(u.m)
-    df['dy_m'] = (EarthLocation(lat=df['Latitude'],lon=df['Longitude'],height=height).y - center.y).to_value(u.m)
-    df['dz_m'] = (EarthLocation(lat=df['Latitude'],lon=df['Longitude'],height=height).z - center.z).to_value(u.m)
+    df['dx_m'] = (EarthLocation(lat=df['Latitude'], lon=df['Longitude'], height=height).x-center.x).to_value(u.m)
+    df['dy_m'] = (EarthLocation(lat=df['Latitude'], lon=df['Longitude'], height=height).y-center.y).to_value(u.m)
+    df['dz_m'] = (EarthLocation(lat=df['Latitude'], lon=df['Longitude'], height=height).z-center.z).to_value(u.m)
 
     return df
 
-def get_baselines(antenna_order,casa_order=True,autocorrs=False):
+def get_baselines(antenna_order, casa_order=True, autocorrs=False):
     # Antenna order is the order of the antennas in the correlator
     # CASA orders the baselines in the reverse direction as the correlator
     # If casa_order is True, the baseline order will be swapped in order to
@@ -61,18 +66,17 @@ def get_baselines(antenna_order,casa_order=True,autocorrs=False):
     nant = len(antenna_order)
     df = get_itrf()
     df_bls = pandas.DataFrame(columns=['bname','x_m','y_m','z_m'])
-    for i in np.arange(1 if not autocorrs else 0,nant):
+    for i in np.arange(1 if not autocorrs else 0, nant):
         for j in np.arange(i if not autocorrs else i+1):
             a1 = antenna_order[i]
             a2 = antenna_order[j]
-            
-            df_bls = df_bls.append({'bname':'{0}-{1}'.format(a1,a2),
-                           'x_m':df.loc[a1]['x_m']-df.loc[a2]['x_m'],
-                           'y_m':df.loc[a1]['y_m']-df.loc[a2]['y_m'],
-                           'z_m':df.loc[a1]['z_m']-df.loc[a2]['z_m']},
-                          ignore_index=True)
+            df_bls = df_bls.append(
+                {'bname':'{0}-{1}'.format(a1,a2),
+                 'x_m':df.loc[a1]['x_m']-df.loc[a2]['x_m'],
+                 'y_m':df.loc[a1]['y_m']-df.loc[a2]['y_m'],
+                 'z_m':df.loc[a1]['z_m']-df.loc[a2]['z_m']},
+                 ignore_index=True)
     if casa_order:
         df_bls = df_bls.iloc[::-1]
-        df_bls.reset_index(inplace=True,drop=True)
+        df_bls.reset_index(inplace=True, drop=True)
     return df_bls
-                                          
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,34 @@
+import random
+import astropy.units as u
 from antpos import utils
-
 from pkg_resources import Requirement, resource_filename
-antposfile = resource_filename(Requirement.parse("dsa110-antpos"), "antpos/data/DSA110_positions_RevD.csv")
+antposfile = resource_filename(Requirement.parse("dsa110-antpos"), "antpos/data/DSA110_positions_RevE.csv")
 
 def test_get():
     df = utils.get_itrf(csvfile=antposfile)
+
+def test_get_itrf():
+    df = utils.get_itrf()
+    ovro_lon = -118.283400*u.deg
+    ovro_lat = 37.233386*u.deg
+    ovro_height = 1188*u.m
+    df = utils.get_itrf(height=ovro_height, latlon_center=(ovro_lat, ovro_lon))
+
+def test_get_baselines():
+    df = utils.get_lonlat()
+    nant = 10
+    antenna_order = [df.index[i] for i in random.sample(range(len(df.index)), nant)]
+    print(antenna_order)
+    df_bls = utils.get_baselines(antenna_order, autocorrs=True, casa_order=False)
+    assert len(df_bls['bname']) == (nant*(nant+1))//2
+    assert df_bls['bname'][0] == '{0}-{0}'.format(antenna_order[0])
+    assert len(df_bls['bname']) == len(df_bls['x_m'])
+    assert len(df_bls['bname']) == len(df_bls['y_m'])
+    assert len(df_bls['bname']) == len(df_bls['z_m'])
+    df_bls = utils.get_baselines(antenna_order, autocorrs=False, casa_order=False)
+    assert len(df_bls['bname']) == (nant*(nant-1))//2
+    assert df_bls['bname'][0] == '{0}-{1}'.format(antenna_order[1],
+                                                   antenna_order[0])
+    df_bls = utils.get_baselines(antenna_order, casa_order=True)
+    assert df_bls['bname'][(nant*(nant-1))//2-1] == '{0}-{1}'.format(antenna_order[1],
+                                                   antenna_order[0])
