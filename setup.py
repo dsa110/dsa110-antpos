caf9dc58bbb59807408cc7db51dc07ffca238373
Dana Simard <dana.simard@astro.caltech.edu>
2020-09-28 12:59:32 -0700

    added revF to package data

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 from version import get_git_version
 
 setup(name='dsa110-antpos',
-      version=get_git_version(),
+      version=0.0,
       url='http://github.com/dsa110/dsa110-antpos/',
       packages=['antpos'],
       package_data={
-          'antpos': ['data/DSA110_positions_RevD.csv','data/DSA110_positions_RevE.csv','data/ant_ids.csv']},
+          'antpos': ['data/DSA110_positions_*.csv', 'data/ant_ids.csv']},
       zip_safe=False)
 
