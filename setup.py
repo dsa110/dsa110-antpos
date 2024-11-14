325ebe0040778cda71f62faea7c1ee9a45164592
Vikram Ravi <vikram@caltech.edu>
2024-11-14 17:12:31 +0000

    using dsautils version.py

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup
-from version import get_git_version
+from dsautils.version import get_git_version
 
 setup(name='dsa110-antpos',
       version=get_git_version(),
       url='http://github.com/dsa110/dsa110-antpos/',
       packages=['antpos'],
+      install_requires=['dsautils'],
       package_data={
           'antpos': ['data/DSA110*.csv', 'data/ant_ids.csv']},
       zip_safe=False)
